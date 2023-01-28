7b3dea2170b79cffea8aadff49c5326d9e2f751e
Venkata-Bhargavi <sikhakolli.v@northeastern.edu>
2023-01-28 18:16:05 -0500

    updated About

--- portfolio.py
+++ portfolio.py
@@ -16,7 +16,7 @@ st.set_page_config(  # Alternate names: setup_page, page, layout
     page_icon= None,  # String, anything supported by st.image, or None.
 )
 load_css()
-
+res_url = "https://drive.google.com/file/d/1YthW-l6dMbe7YmnFPKhNIWiixroTNdaA/view?usp=sharing"
 def load_lottiefile(filepath:str):
     with open(filepath,"r") as f:
         return json.load(f)
@@ -277,7 +277,7 @@ with pro:
          scala_url = "https://github.com/bhargavi31/CommandLineTool/tree/master/CommandLineTool"
          st.markdown("")
          st.markdown("")
-         
+
          # git_button = st.button("**Github 📊**")
          # if git_button:
          #     open_link(scala_url)
@@ -367,13 +367,22 @@ with Blog:
 with about:
     st.markdown("")
     st.markdown("")
+    st.header("Hi Again!")
+    st.markdown(
+        f"I am Information Systems Graduate student from Northeastern University Boston. Prior to this I worked as a Research Engineer at BUDDI AI (More details about my role and responsibilities can be found in my [Resume]({res_url})). I have pursued M.Tech Integrated Software Engineering (Undergrad + Master's, a five year integrated course) from Vellore Institute of Technology, Vellore India.")
+
     col1_1,col1_2 = st.columns([1,2])
     with col1_1:
-        st.header("Contact Information")
+      st.header("Contact Information")
         # st.image("Utils/Images/linkedin.png",width= 20)
+
+
+    st.markdown("<p style='font-size: 20px'><span style='color: grey;'>Would you like to disscuss a project or a tech topic, or just want to say Hi? drop an email to</span> bhargavi.sikhakolli31@gmail.com</p>", unsafe_allow_html=True)
+    st.markdown("<p style='font-size: 20px'><span style='color: grey;'>Open to Opportunities : YES</span></p>",
+                unsafe_allow_html=True)
+
     col2_1,col2_2 = st.columns([1,50])
     with col2_1:
         st.image("Utils/Images/linkedin.png", width=20)
     with col2_2:
         st.markdown("[LinkedIn](https://www.linkedin.com/in/bhargavi-sikhakolli-9ab281117/)")
-
